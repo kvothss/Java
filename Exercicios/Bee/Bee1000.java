de652cfbd238a520a2b8e8d18bcf67d8ed4f57b5
Marcos Souza <121893069+kvothss@users.noreply.github.com>
2025-06-18 13:19:36 -0300

    Delete Exercicios/Bee directory
    Renomeando diretório

--- Exercicios/Bee/Bee1000.java
+++
@@ -1,7 +0,0 @@
-package Exercicios.Bee;
-
-public class Bee1000 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- Exercicios/Bee/ConversaoCelsius.java
+++
@@ -1,7 +0,0 @@
-package Exercicios.Bee;
-
-public class ConversaoCelsius {
-    public static void main(String[] args) {
-
-    }
-}
